b55d551b0528339f3cd656af6fb537189ab9445c
CQUT-rjgc-zys <2200642826@qq.com>
2025-03-06 00:05:48 +0800

    null

--- src/main/java/org/bysj/mapper/DrugInformationMapper.java
+++ src/main/java/org/bysj/mapper/DrugInformationMapper.java
@@ -1,11 +1,13 @@
 package org.bysj.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.bysj.entity.DrugInformationEntity;
 
 /**
  * 药品信息 Mapper 接口
  */
+@Mapper
 public interface DrugInformationMapper extends BaseMapper<DrugInformationEntity> {
     // 可以在这里添加自定义查询方法
 }
